7bbb76dc9b085e3a3b452b35965843a8cef01d70
Michael Anderson <manderson3992@yahoo.com>
2022-08-03 15:26:29 -0400

    Migrations with Knex

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,17 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+const path = require("path")
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
 module.exports = {
 
   development: {
-    client: 'protresqlsqlite3',
+    client: 'postgresql',
     connection: DATABASE_URL,
-  }
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+  },
 };
